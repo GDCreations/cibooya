490b3a8a0853edc6f49aab6cd3d477c71aaa28f2
GdcJanaka <janaka@gdcreations.com>
2019-09-17 16:26:08 +0530

    dashboard

---
+++ application/views/user/dashboard.php
@@ -0,0 +1,15 @@
+<!-- START PAGE HEADING -->
+<div class="app-heading-container app-heading-bordered bottom">
+    <ul class="breadcrumb">
+        <li class="active">Dashboard</li>
+    </ul>
+</div>
+<!-- END PAGE HEADING -->
+<!-- START PAGE CONTAINER -->
+<div class="container">
+    <div class="block">
+
+    </div>
+</div>
+<!-- END PAGE CONTAINER -->
+
