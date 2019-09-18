1c2d86d3954cc17548a08f13d16036a74decbbfd
gemunu <gemunu@gdcreations.com>
2019-09-18 15:26:17 +0530

    some login logout changes

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,19 +1,56 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class User extends CI_Controller {
+class User extends CI_Controller
+{
 
-	public function index()
-	{
-	    //Active Page Id
+    function __construct()
+    {
+        parent::__construct();
+
+        // Deletes cache for the currently requested URI
+        $this->output->delete_cache();
+
+        //$this->load->library('Pdf'); // Load library
+        //$this->pdf->fontpath = 'font/'; // Specify font folder
+
+        $this->load->database();                // load database
+        $this->load->model('Generic_model');    // load model
+        //$this->load->model('User_model');       // load model
+
+        date_default_timezone_set('Asia/Colombo');
+
+        if (!empty($_SESSION['userId'])) {
+
+            $user = $this->Generic_model->chckVlidUsr();    // IF CHECK VALIDE USER SESSION
+            if ($user > 0) {
+
+            } else {
+                $this->session->sess_destroy();
+                redirect('/');
+            }
+        } else {
+            redirect('/');
+        }
+    }
+
+    public function index()
+    {
+        //Active Page Id
         $data['acm'] = '';
         $data['acp'] = 'dashbrd';
-		$this->load->view('common/tmpHeader');
+        $this->load->view('common/tmpHeader');
         $this->load->view('user/common/userHeader');
+
 
         $this->load->view('user/dashboard');
 
-        $this->load->view('common/tmpFooter',$data);
+        $this->load->view('common/tmpFooter', $data);
 
-	}
+    }
+
+
+
+
+
 }
