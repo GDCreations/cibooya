be904b200b12f0019ad1eab2ca107c18cec221d2
gemunu <gemunu@gdcreations.com>
2019-09-19 14:29:16 +0530

    form-control height change

---
+++ application/views/admin/companyDetails.php
@@ -0,0 +1,29 @@
+<!-- START PAGE HEADING -->
+<div class="app-heading-container app-heading-bordered bottom">
+    <ul class="breadcrumb">
+        <li class="active">Admin Dashboard</li>
+    </ul>
+</div>
+<div class="app-heading app-heading-bordered app-heading-page">
+    <div class="icon icon-lg">
+        <span class="icon-home" style="color: #e69c0f;"></span>
+    </div>
+    <div class="title">
+        <h1>Admin Dashboard</h1>
+        <p>Subtitle</p>
+    </div>
+</div>
+<!-- END PAGE HEADING -->
+
+<!-- START PAGE CONTAINER -->
+<div class="container">
+
+    <div class="block">
+
+
+    </div>
+
+
+</div>
+<!-- END PAGE CONTAINER -->
+
