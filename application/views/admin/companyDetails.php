aa723e98bbe2e9e004bb483619560398e6787009
gemunu <gemunu@gdcreations.com>
2019-09-19 17:05:06 +0530

    system branding

--- application/views/admin/companyDetails.php
+++ application/views/admin/companyDetails.php
@@ -20,30 +20,34 @@
 <div class="container">
     <div class="block">
 
-        <form class="form-horizontal" name="" method="post">
+        <form class="form-horizontal" id="brandingEdt" method="post">
             <div class="col-md-6">
                 <div class="form-group">
                     <label class="col-md-4 control-label">Company Name</label>
                     <div class="col-md-8">
-                        <input type="text" class="form-control" value="<?= $compInfo[0]->cmne ?>" name="cmne">
+                        <input type="text" class="form-control" value="<?= $compInfo[0]->cmne ?>" name="cmne"
+                               id="cmne"/>
                     </div>
                 </div>
                 <div class="form-group">
                     <label class="col-md-4 control-label">Company Address</label>
                     <div class="col-md-8">
-                        <input type="text" class="form-control" value="<?= $compInfo[0]->cadd ?>" name="cadd">
+                        <input type="text" class="form-control" value="<?= $compInfo[0]->cadd ?>" name="cadd"
+                               id="cadd"/>
                     </div>
                 </div>
                 <div class="form-group">
                     <label class="col-md-4 control-label">Company Phone</label>
                     <div class="col-md-8">
-                        <input type="text" class="form-control" value="<?= $compInfo[0]->ctel ?>" name="ctel">
+                        <input type="text" class="form-control" value="<?= $compInfo[0]->ctel ?>" name="ctel"
+                               id="ctel"/>
                     </div>
                 </div>
                 <div class="form-group">
                     <label class="col-md-4 control-label">System Link</label>
                     <div class="col-md-8">
-                        <input type="text" class="form-control" value="<?= $compInfo[0]->syln ?>" name="syln">
+                        <input type="text" class="form-control" value="<?= $compInfo[0]->syln ?>" name="syln"
+                               id="syln"/>
                     </div>
                 </div>
             </div>
@@ -51,30 +55,34 @@
                 <div class="form-group">
                     <label class="col-md-4 control-label">System Name</label>
                     <div class="col-md-8">
-                        <input type="text" class="form-control" value="<?= $compInfo[0]->synm ?>" name="synm">
+                        <input type="text" class="form-control" value="<?= $compInfo[0]->synm ?>" name="synm"
+                               id="synm"/>
                     </div>
                 </div>
                 <div class="form-group">
                     <label class="col-md-4 control-label">Email</label>
                     <div class="col-md-8">
-                        <input type="text" class="form-control" value="<?= $compInfo[0]->ceml ?>" name="ceml">
+                        <input type="text" class="form-control" value="<?= $compInfo[0]->ceml ?>" name="ceml"
+                               id="ceml"/>
                     </div>
                 </div>
                 <div class="form-group">
                     <label class="col-md-4 control-label">Register Date</label>
                     <div class="col-md-8">
-                        <input type="text" class="form-control" value="<?= $compInfo[0]->regd ?>" name="regd">
+                        <input type="text" class="form-control" value="<?= $compInfo[0]->regd ?>" name="regd"
+                               id="regd"/>
                     </div>
                 </div>
                 <div class="form-group">
                     <label class="col-md-4 control-label">Web Mail Link</label>
                     <div class="col-md-8">
-                        <input type="text" class="form-control" value="<?= $compInfo[0]->wbml ?>" name="wbml">
+                        <input type="text" class="form-control" value="<?= $compInfo[0]->wbml ?>" name="wbml"
+                               id="wbml"/>
                     </div>
                 </div>
 
                 <div class="form-group margin-top-20">
-                    <button class="btn btn-success pull-right" type="submit">Submit</button>
+                    <button class="btn btn-success pull-right" type="button" id="save">Submit</button>
                 </div>
             </div>
         </form>
@@ -86,144 +94,78 @@
 <script>
 
     $().ready(function () {
-        $('#add_sup_form').validate({
+        $('#brandingEdt').validate({
             rules: {
-                name:{
-                    required : true
-                },
-                addr:{
+                cmne: {
                     required: true
                 },
-                mobi:{
-                    required: true,
+                cadd: {
+                    required: true
+                },
+                ctel: {
                     digits: true,
                     minlength: 10,
                     maxlength: 10,
-                    remote: {
-                        url: "<?= base_url(); ?>Stock/chk_mobile",
-                        type: "post",
-                        data: {
-                            mobi: function () {
-                                return $("#mobi").val();
-                            },
-                            stat: 0
-                        }
-                    }
                 },
-                tele:{
-                    digits: true,
-                    minlength: 10,
-                    maxlength: 10,
-                    remote: {
-                        url: "<?= base_url(); ?>Stock/chk_mobile",
-                        type: "post",
-                        data: {
-                            mobi: function () {
-                                return $("#tele").val();
-                            },
-                            stat: 0
-                        }
-                    }
-                },
-                email: {
+                ceml: {
                     required: true,
                     email: true
                 },
-                bnknm:{
-                    notEqual: 0
-                },
-                bnkbr:{
-                    notEqual: 0
-                },
-                acno:{
+                syln: {
                     required: true,
-                    digits: true,
-                    minlength: 8,
-                    remote: {
-                        url: "<?= base_url(); ?>Stock/chk_bnkAcno",
-                        type: "post",
-                        data: {
-                            acno: function () {
-                                return $("#acno").val();
-                            },
-                            stat: 0
-                        }
-                    }
-                }
+                },
+                synm: {
+                    required: true,
+                },
             },
             messages: {
-                name:{
-                    required : "Enter supplier name"
+                cmne: {
+                    required: "Enter supplier name"
                 },
-                addr:{
+                addr: {
                     required: "Enter supplier address"
                 },
-                mobi:{
-                    required: "Enter mobile number",
-                    digits: "Only numbers are allowed",
-                    minlength: "Please enter 10 digits number",
-                    maxlength: "Please enter 10 digits number",
-                    remote: "This number is already added"
-                },
-                tele:{
-                    digits: "Only numbers are allowed",
-                    minlength: "Please enter 10 digits number",
-                    maxlength: "Please enter 10 digits number",
-                    remote: "This number is already added"
-                },
-                email: {
-                    required: "Enter email address",
-                    email: "Please enter valid email address"
-                },
-                bnknm:{
-                    notEqual: "Select a bank"
-                },
-                bnkbr:{
-                    notEqual: "Select a bank branch"
-                },
-                acno:{
-                    required: "Enter bank account number",
-                    digits: "Only numbers are allowed",
-                    minlength: "Minumum length is 8 digits",
-                    remote: "This account number is already added"
-                }
             }
         });
     });
 
     //Add New Supplier
-    $('#add_sup_btn').click(function (e) {
+    $('#save').click(function (e) {
         e.preventDefault();
-        if ($('#add_sup_form').valid()) {
-            $('#add_sup_btn').prop('disabled',true);
+        if ($('#brandingEdt').valid()) {
+
+            $('#save').prop('disabled', true);
+
             swal({
                 title: "Processing...",
-                text: "Supplier's data saving..",
+                text: "Branding data saving..",
                 imageUrl: "<?= base_url() ?>assets/img/loading.gif",
                 showConfirmButton: false
             });
 
             jQuery.ajax({
                 type: "POST",
-                url: "<?= base_url(); ?>Stock/supp_Regist",
-                data: $("#add_sup_form").serialize(),
+                url: "<?= base_url(); ?>admin/updateBranding",
+                data: $("#brandingEdt").serialize(),
                 dataType: 'json',
                 success: function (data) {
-                    swal({title: "", text: "Registration Success!", type: "success"},
+                    swal({title: "", text: "Update Success!", type: "success"},
                         function () {
-                            $('#add_sup_btn').prop('disabled',false);
-                            clear_Form('add_sup_form');
+                            $('#save').prop('disabled', false);
+                            clear_Form('brandingEdt');
                             $('#modal-add').modal('hide');
                         });
                 },
                 error: function (data, textStatus) {
-                    swal({title: "Registration Failed", text: textStatus, type: "error"},
+                    swal({title: "Update Failed", text: textStatus, type: "error"},
                         function () {
                             location.reload();
                         });
                 }
             });
+
         }
     });
+
 
 </script>
