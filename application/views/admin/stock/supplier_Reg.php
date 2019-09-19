672c1de9f737ce945818310af10fb15ff0bf689d
GdcJanaka <janaka@gdcreations.com>
2019-09-19 15:25:15 +0530

    Supplier Registration 2

--- application/views/admin/stock/supplier_Reg.php
+++ application/views/admin/stock/supplier_Reg.php
@@ -434,24 +434,4 @@
             }
         });
     }
-    
-    //FORM CLEARING FUNTION
-    function clear_Form(form) {
-        $("#" + form).trigger("reset");
-
-        //get selector elements
-        var nodes = $('#' + form).find(
-            "select"
-        ).val(0);
-
-        //Reseting selector items
-        nodes.map(function () {
-            var node2 = $("#"+$(this).attr('id')).prev(); //get previous element of selector element
-            node2.children().children().removeClass('selected'); //remove selected classes
-            node2.children().children().first().addClass('selected'); //add selected class
-            var selected = node2.children().children().first().children().children().first().html(); //get selected valus
-            node2.prev().prop('title',selected); //set selected value as title
-            node2.prev().children().first().html(selected); //set selected value as <li> element
-        });
-    }
 </script>
