0d765172af70ce2ccc2a7b2e05a1e91c24444a5c
gemunu <gemunu@gdcreations.com>
2019-09-20 16:32:08 +0530

    Recent Activity

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -16,7 +16,7 @@ class Admin extends CI_Controller
 
         $this->load->database();                // load database
         $this->load->model('Generic_model');    // load model
-        //$this->load->model('User_model');       // load model
+        $this->load->model('Admin_model');      // load model
 
         date_default_timezone_set('Asia/Colombo');
 
@@ -45,8 +45,33 @@ class Admin extends CI_Controller
         $this->load->view('admin/adminDash');
 
         $this->load->view('common/tmpFooter', $data);
-
     }
+
+    // BEGIN COMMON DATA LOADING
+// Get user data
+    public function getUser()
+    {
+        $bid = $this->input->post('brid');
+        $uslv = $this->input->post('uslv');
+
+        $this->db->select("auid,fnme,lnme,brch");
+        $this->db->from("user_mas");
+        $this->db->where('user_mas.usmd  != 1');
+        $this->db->where('user_mas.stat', 1);
+        if ($bid != 'all') {
+            $this->db->where('user_mas.brch ', $bid);
+        }
+        if ($uslv != 'all') {
+            $this->db->where('user_mas.usmd ', $uslv);
+        }
+
+        $query = $this->db->get();
+        echo json_encode($query->result());
+    }
+
+
+    // END COMMON DATA LOADING
+
 
     // COMPANY BRANDING
     public function branding()
@@ -139,9 +164,7 @@ class Admin extends CI_Controller
         }
     }
 
-
-
-    // SYSTEM POLICY
+    // RECENTY ACTIVITY
     public function rcntAct()
     {
         $data['acm'] = ''; //Module
@@ -150,10 +173,51 @@ class Admin extends CI_Controller
         $this->load->view('common/tmpHeader');
         $this->load->view('admin/common/adminHeader');
 
-        $dataArr['policyinfo'] = $this->Generic_model->getData('sys_policy', '', '');
+        $dataArr['branchinfo'] = $this->Generic_model->getBranch();
+        $dataArr['uslvlinfo'] = $this->Generic_model->getData('user_level', '', "stat = 1 AND id != 1");
 
-        $this->load->view('admin/systmPolicy', $dataArr);
+        $this->load->view('admin/recentActivity', $dataArr);
         $this->load->view('common/tmpFooter', $data);
+    }
+
+// Recent activity search
+    function srchActivit()
+    {
+        $result = $this->Admin_model->get_recntDtils();
+        $data = array();
+        $i = $_POST['start'];
+
+        foreach ($result as $row) {
+            //$img = "<img src='../uploads/userimg/" . $row->uimg . "'  class='sm-image' title='" . $row->usnm . "' style='width: 27px;height: 27px' />";
+
+            $sub_arr = array();
+            $sub_arr[] = ++$i;
+            $sub_arr[] =  $row->usnm . ' (' . $row->brcd . ')';
+            if ($row->pnm != '' && $row->func != '') {
+                $sub_arr[] = $row->pnm . ' --> ' . $row->func;
+            } else if ($row->pnm != '') {
+                $sub_arr[] = $row->pnm;
+            } else if ($row->func != '') {
+                $sub_arr[] = $row->func;
+            }
+            $date = date_create($row->lgdt);
+            $sub_arr[] = date_format($date, 'g:i:s A \o\n l j F Y ');
+            //$sub_arr[] = date_format($date, 'g:ia \o\n l jS F Y');
+            $sub_arr[] = $row->lgip;
+
+            $data[] = $sub_arr;
+        }
+
+        $output = array(
+            "draw" => $_POST['draw'],
+            "recordsTotal" => $this->Admin_model->all_recnt(),
+            "recordsFiltered" => $this->Admin_model->filtered_recnt(),
+            "data" => $data,
+        );
+        echo json_encode($output);
+        //$funcPerm = $this->Generic_model->getFuncPermision('recn_actv');
+        //$this->Log_model->userFuncLog($funcPerm[0]->pgid, 'View Recent Activity');
+
     }
 
 
