e1f6981bd19d3fe271e60b339722071be66214ab
gemunu <gemunu@gdcreations.com>
2019-09-20 13:49:01 +0530

    system policy updated

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -48,6 +48,7 @@ class Admin extends CI_Controller
 
     }
 
+    // COMPANY BRANDING
     public function branding()
     {
         $data['acm'] = 'gnrl'; //Module
@@ -61,21 +62,18 @@ class Admin extends CI_Controller
         $this->load->view('admin/companyDetails', $dataArr);
 
         $this->load->view('common/tmpFooter', $data);
-
     }
 
     function updateBranding()
     {
+        $this->db->trans_begin(); // SQL TRANSACTION START
+
         $data_ar1 = array(
             'cmne' => $this->input->post('cmne'),
             'synm' => strtoupper($this->input->post('synm')),
             'cadd' => ucwords(strtolower($this->input->post('cadd'))),
             'ctel' => $this->input->post('ctel'),
             'ceml' => $this->input->post('ceml'),
-            //'chot' => $this->input->post('mobi'),
-            //'rplg' => $rplg,
-            //'cplg' => $cplg,
-            //'regn' => strtoupper($this->input->post('rgno')),
             'regd' => $this->input->post('regd'),
             'syln' => $this->input->post('syln'),
             'wbml' => $this->input->post('wbml'),
@@ -90,11 +88,56 @@ class Admin extends CI_Controller
 
         // $funcPerm = $this->Generic_model->getFuncPermision('brand');
         // $this->Log_model->userFuncLog($funcPerm[0]->pgid, 'Company Details Update ');
-        if (count($result22) > 0) {
-            echo json_encode(true);
-        } else {
+
+        if ($this->db->trans_status() === FALSE) {
+            $this->db->trans_rollback();
             echo json_encode(false);
+        } else {
+            $this->db->trans_commit(); // SQL TRANSACTION END
+            echo json_encode(true);
         }
     }
 
+    // SYSTEM POLICY
+    public function policyMng()
+    {
+        $data['acm'] = ''; //Module
+        $data['acp'] = 'policyMng'; //Page
+
+        $this->load->view('common/tmpHeader');
+        $this->load->view('admin/common/adminHeader');
+
+        $dataArr['policyinfo'] = $this->Generic_model->getData('sys_policy', '', '');
+
+        $this->load->view('admin/systmPolicy', $dataArr);
+        $this->load->view('common/tmpFooter', $data);
+    }
+
+    function updatePolicy()
+    {
+        $this->db->trans_begin(); // SQL TRANSACTION START
+
+        $date = date('Y-m-d H:i:s');
+
+        $digey = $this->input->post('digey');   // DIGITAL EYE CODE ENABLE/ DISABLE
+        $eycd = $this->input->post('eycd');     // DIGITAL EYE CODE
+
+        $_01 = array('pov3' => $eycd, 'post' => $digey, 'mdby' => $_SESSION['userId'], 'mddt' => $date);
+
+
+        $this->Generic_model->updateData('sys_policy', $_01, array('poid' => 1));
+
+
+        //$funcPerm = $this->Generic_model->getFuncPermision('policyMng');
+        //$this->Log_model->userFuncLog($funcPerm[0]->pgid, 'System Policy Update');
+
+
+        if ($this->db->trans_status() === FALSE) {
+            $this->db->trans_rollback();
+            echo json_encode(false);
+        } else {
+            $this->db->trans_commit(); // SQL TRANSACTION END
+            echo json_encode(true);
+        }
+    }
 }
